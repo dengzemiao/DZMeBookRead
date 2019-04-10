044465741877fffd49fa440348c041a9eb749143
dengzemiao <dengzemiao@dengzemiaodemacbook-pro.local>
2019-04-10 11:59:41 +0800

    优化背景显示！

--- DZMeBookRead/DZMeBookRead/controller/DZMReadBGController.swift
+++ DZMeBookRead/DZMeBookRead/controller/DZMReadBGController.swift
@@ -11,10 +11,10 @@ import UIKit
 class DZMReadBGController: UIViewController {
 
     /// 目标视图(无值则跟阅读背景颜色保持一致)
-    weak var targetView:UIView?
+    weak var targetView:UIView!
     
-    /// imageView
-    private(set) var imageView:UIImageView!
+    /// 图片
+    private var imageView:UIImageView!
     
     override func viewDidLoad() {
         
@@ -27,8 +27,47 @@ class DZMReadBGController: UIViewController {
         imageView.layer.transform = CATransform3DMakeRotation(CGFloat.pi, 0, 1, 0)
         view.addSubview(imageView)
         
+        // funcOne()
+        
+        funcTwo()
+    }
+    
+    // MARK: 方式一
+    
+    /// 方式一
+    private func funcOne() {
+        
         // 展示图片
-        if targetView != nil { imageView.image = ScreenCapture(targetView) }
+        if targetView != nil {
+            
+            imageView.image = ScreenCapture(targetView)
+        }
+    }
+    
+    // MARK: 方式二
+    
+    /// 方式二
+    private func funcTwo() {
+        
+        // 展示图片
+        if targetView != nil {
+            
+            let rect = targetView.frame
+            
+            UIGraphicsBeginImageContextWithOptions(rect.size, true, 0.0)
+            
+            let context = UIGraphicsGetCurrentContext()
+            
+            let transform = CGAffineTransform(a: -1.0, b: 0.0, c: 0.0, d: 1.0, tx: rect.size.width, ty: 0.0)
+            
+            context?.concatenate(transform)
+            
+            targetView.layer.render(in: context!)
+            
+            imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+            
+            UIGraphicsEndImageContext()
+        }
     }
 
     override func didReceiveMemoryWarning() {
