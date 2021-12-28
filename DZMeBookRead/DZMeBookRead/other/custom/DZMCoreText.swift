9cfaf762e494f16a645d3b0312a60d327b2ec986
dengzemiao <dengzemiaovip@163.com>
2021-12-28 16:13:33 +0800

    支持 DZMCoreText 获取指定页(内容)获取所有断尾 NSRange 或者 CGRect，方便做断尾评论功能

--- DZMeBookRead/DZMeBookRead/other/custom/DZMCoreText.swift
+++ DZMeBookRead/DZMeBookRead/other/custom/DZMCoreText.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Darwin
+import Foundation
 
 class DZMCoreText: NSObject {
     
@@ -60,13 +61,12 @@ class DZMCoreText: NSObject {
         
         return rangeArray
     }
-
     
     /// 获得触摸位置文字的Location
     ///
     /// - Parameters:
     ///   - point: 触摸位置
-    ///   - frameRef: CTFrame
+    ///   - frameRef: 内容 CTFrame
     /// - Returns: 触摸位置的Index
     @objc class func GetTouchLocation(point:CGPoint, frameRef:CTFrame?) ->CFIndex {
         
@@ -86,7 +86,7 @@ class DZMCoreText: NSObject {
     ///
     /// - Parameters:
     ///   - point: 触摸位置
-    ///   - frameRef: CTFrame
+    ///   - frameRef: 内容 CTFrame
     /// - Returns: 一行的 NSRange
     @objc class func GetTouchLineRange(point:CGPoint, frameRef:CTFrame?) ->NSRange {
         
@@ -99,8 +99,8 @@ class DZMCoreText: NSObject {
     ///
     /// - Parameters:
     ///   - point: 触摸位置
-    ///   - frameRef: CTFrame
-    ///   - content: 当前的页的正文内容，传了则获取长按的段落 NSRange，没传则获取一行文字的 NSRange
+    ///   - frameRef: 内容 CTFrame
+    ///   - content: 内容字符串，传了则获取长按的段落 NSRange，没传则获取一行文字的 NSRange
     /// - Returns: 一个段落的 NSRange || 一行文字的 NSRange
     @objc class func GetTouchParagraphRange(point:CGPoint, frameRef:CTFrame?, content:String? = nil) ->NSRange {
         
@@ -174,7 +174,7 @@ class DZMCoreText: NSObject {
     ///
     /// - Parameters:
     ///   - point: 触摸位置
-    ///   - frameRef: CTFrame
+    ///   - frameRef: 内容 CTFrame
     /// - Returns: CTLine
     @objc class func GetTouchLine(point:CGPoint, frameRef:CTFrame?) ->CTLine? {
         
@@ -233,10 +233,61 @@ class DZMCoreText: NSObject {
         return line
     }
     
+    /// 获取内容所有段落尾部 NSRange
+    ///
+    /// - Parameters:
+    ///   - frameRef: 内容 CTFrame
+    ///   - content: 内容字符串，也就是生成 frameRef 的正文内容
+    /// - Returns: [NSRange] 传入内容的所有断尾 NSRange
+    @objc class func GetParagraphEndRanges (frameRef:CTFrame?, content:String?) -> [NSRange] {
+        
+        var ranges:[NSRange] = []
+        
+        if (frameRef != nil && content != nil) {
+            
+            let lines:[CTLine] = CTFrameGetLines(frameRef!) as! [CTLine]
+            
+            for (index, line) in lines.enumerated() {
+                
+                let lintRange = GetLineRange(line: line)
+                
+                let lineString = content?.substring(lintRange)
+                
+                let isEnd = lineString?.contains("\n") ?? false
+                
+                if (isEnd) { ranges.append(lintRange) }
+            }
+        }
+        
+        return ranges
+    }
+    
+    /// 获取内容所有段落尾部 CGRect
+    ///
+    /// - Parameters:
+    ///   - frameRef: 内容 CTFrame
+    ///   - content: 内容字符串，也就是生成 frameRef 的正文内容
+    /// - Returns: [CGRect] 传入内容的所有断尾 CGRect
+    @objc class func GetParagraphEndRects (frameRef:CTFrame?, content:String?) -> [CGRect] {
+        
+        var rects:[CGRect] = []
+        
+        let ranges = GetParagraphEndRanges(frameRef: frameRef, content: content)
+        
+        for range in ranges {
+            
+            let rect = GetRangeRects(range: range, frameRef: frameRef, content: content)
+            
+            rects += rect
+        }
+        
+        return rects
+    }
+    
     /// 通过 range 返回字符串所覆盖的位置 [CGRect]
     ///
     /// - Parameter range: NSRange
-    /// - Parameter frameRef: CTFrame
+    /// - Parameter frameRef: 内容 CTFrame
     /// - Parameter content: 内容字符串(有值则可以去除选中每一行区域内的 开头空格 - 尾部换行符 - 所占用的区域,不传默认返回每一行实际占用区域)
     /// - Returns: 覆盖位置
     @objc class func GetRangeRects(range:NSRange, frameRef:CTFrame?, content:String? = nil) -> [CGRect] {
@@ -335,7 +386,7 @@ class DZMCoreText: NSObject {
     /// 通过 range 获得合适的 MenuRect
     ///
     /// - Parameter rects: [CGRect]
-    /// - Parameter frameRef: CTFrame
+    /// - Parameter frameRef: 内容 CTFrame
     /// - Parameter viewFrame: 目标ViewFrame
     /// - Parameter content: 内容字符串
     /// - Returns: MenuRect
